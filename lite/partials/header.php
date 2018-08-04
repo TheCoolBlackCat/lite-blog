dadac2d898fb51dc9411e1bbbe2ed8abf1c76845
Tim <thecoolblackcat@timothyclark.co.uk>
2018-08-05 00:46:54 +0100

    Added Favicon

--- lite/partials/header.php
+++ lite/partials/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8" />
     <title>Blog</title>
     <link rel="stylesheet" type="text/css" href="/lite/lite.css" />
+    <?php include("favicon.php") ?>
   </head>
   <body>
     
